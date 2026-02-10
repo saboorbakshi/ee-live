bc3eb8552059ab2b452fadf25fdc7895741449f0
saboorbakshi <saboor.bakshi@uwaterloo.ca>
2026-02-10 03:23:36 -0500

    style: line height and font weight

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -76,9 +76,9 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center font-sans">
       <main className="flex min-h-screen w-full max-w-xl flex-col py-8 sm:py-14 px-5 gap-12 sm:gap-16">
-        <div className="-mb-2">
-          <p className="text-4xl sm:text-5xl font-medium mb-6">Canada Express Entry Statistics</p>
-          <p className="mb-4">
+        <div>
+          <p className="text-4xl sm:text-5xl font-medium mb-8">Canada Express Entry Statistics</p>
+          <p className="mb-6">
             All Express Entry draws since 2020 are tracked and automatically updated here to help you better understand
             your position based on your{" "}
             <ExternalLink href="https://www.canada.ca/en/immigration-refugees-citizenship/services/immigrate-canada/express-entry/check-score.html">
@@ -90,7 +90,7 @@ export default function Home() {
             </ExternalLink>.
             The charts below break this data into three views:
           </p>
-          <ol className="list-decimal ml-6 space-y-1 mb-4">
+          <ol className="list-decimal ml-6 space-y-1 mb-6">
             <li>
               The lowest CRS score required for an invitation in each draw, shown chronologically to highlight trends over time.
             </li>
